616ac29d46b59b7f44a1b3751fc1903eb45058d9
gozu66 <gozu66@gmail.com>
2016-10-05 00:27:11 +0100

    Setting up git repository

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+    public short speed;
+    public short jumpForce;
+
+    private Rigidbody2D rbody;
+
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+        Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if(inputDirection.sqrMagnitude != 0)
+        {
+            Vector2 newVelocity = new Vector2(inputDirection.x * speed, rbody.velocity.y);
+            rbody.velocity = newVelocity;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        }
+	}
+}
