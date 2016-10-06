3e674700d1f0943cb54187472aa655a3d87da965
gozu66 <gozu66@gmail.com>
2016-10-06 03:25:23 +0100

    player movement setup

--- Assets/_RB_Scripts/playerMove.cs
+++ Assets/_RB_Scripts/playerMove.cs
@@ -1,30 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerMove : MonoBehaviour {
+public class playerMove : MonoBehaviour 
+{
+	public float maxSpeed, jumpForce;
 
-    public short speed;
-    public short jumpForce;
+	bool isMoving, facingLeft, jumpPressed = false, onSlope;
 
-    private Rigidbody2D rbody;
+	//public LayerMask ground;
+	//public Transform groundcheck;
 
-	void Start () {
-        rbody = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
+    Rigidbody2D rBody;
+    Collider2D col;
 
-        Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), 0);
+	public float range;
 
-        if(inputDirection.sqrMagnitude != 0)
+	void Start ()
+	{
+        rBody = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+
+        InvokeRepeating("checkGround", 0.0025f, 0.0025f);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0) { isMoving = true; } else { isMoving = false; }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            Vector2 newVelocity = new Vector2(inputDirection.x * speed, rbody.velocity.y);
-            rbody.velocity = newVelocity;
+            jump();
+        }
+        else
+        {
+            jumpPressed = false;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetAxis("Horizontal") != 0)
         {
-            rbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
-        }
+            float move = Input.GetAxis("Horizontal");
+
+            rBody.velocity = new Vector2(move * maxSpeed, rBody.velocity.y);   
+
+            if (move > 0 && facingLeft)
+                Flip();
+            else if (move < 0 && !facingLeft)
+                Flip();
+        }			
 	}
+
+	void Update()
+	{
+        //Ray groundCheckRay;
+        //RaycastHit2D hit;
+
+        //if(Physics2D.Raycast(transform.position, -Vector2.up, hit))
+        //RaycastHit2D hit = Physics2D.Linecast()
+	}
+
+	void Flip()
+	{
+		facingLeft = !facingLeft;
+		Vector3 myScale = transform.localScale;
+		myScale.x *= -1;
+		transform.localScale = myScale;
+	}
+
+	void jump()
+	{
+		if(isGrounded)
+		{
+            rBody.AddForce((Vector2.up * jumpForce), ForceMode2D.Force);
+			jumpPressed = true;
+		}
+	}
+
+    bool isGrounded;
+    void checkGround()
+    {
+        Collider2D[] _ground = new Collider2D[1];
+        Physics2D.OverlapAreaNonAlloc(col.bounds.max, col.bounds.min, _ground);
+
+        foreach (Collider2D _col in _ground)
+        {
+            if (_col != null && !_col.isTrigger)
+            {
+                isGrounded = true;
+            } else {
+                isGrounded = false;
+            }
+        }
+    }
+
 }
